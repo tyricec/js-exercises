f66f0c7ac4f18813db0e98819f68f377a91b48ab
Tyrice Clark <tyricec@civiconnect.com>
2017-10-04 11:18:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Exercises
+---------
+
+Exercises I created for praciticing various programming fundamentals in JS.
---
+++ multiplication-table/main.js
@@ -0,0 +1,5 @@
+// Create a multiplication table
+// The table should have a title, Multiplication Table for 1 - 12
+// Hint: How the html should look is provided in result.html. Understand how this works first before writing the javascript to recreate it.
+// Hint: Will require nested for loop. Search nested for loops 
+// Extra Credit: Alternate colors between each row as shown in result.html
