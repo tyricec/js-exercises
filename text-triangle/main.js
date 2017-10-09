4507495ea1d2e9e1c2468f5e55d90092c10a34c8
Tyrice Clark <tyricec@civicresource.com>
2017-10-08 21:04:48 -0700

    Add triangle exercise

---
+++ text-triangle/main.js
@@ -0,0 +1,18 @@
+/*
+ * Write for loop(s) to log the following triangle in a console.
+ *
+ * *
+ * **
+ * ***
+ * ****
+ * ***** 
+ * ******
+ * *******
+ * ********
+ * *********
+ * **********
+ * 
+ * Hint: You only need one console.log
+ * 
+ * Extra Credit: Print another triangle using only a while loop
+ */
